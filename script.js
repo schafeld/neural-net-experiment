cdee0e9720538ec71d754ce92b20f097a1de7899
Glitch (neural-net-experiment) <none>
2019-01-08 19:14:50 +0000

    set up first neural net configuration

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var brain = brain || {};
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+const trainingData = [
+  { input: [0, 0], output: [0] },
+  { input: [0, 1], output: [1] },
+  { input: [1, 0], output: [1] },
+  { input: [1, 1], output: [0] }
+];
+
+net.train(trainingData);
+
+// console.log(net.run([0, 0]));
+var output = net.run([0, 0]);
+
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<p>Output: ${output}</p>`;
